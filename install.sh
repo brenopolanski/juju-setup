7567fa25c48cc12d289fce28c5a8657b7282dbc8
brenopolanski <breno.polanski@gmail.com>
2016-12-07 17:15:56 -0300

    Update setup

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ answer=$(zenity  --list  --text "Choose the packages you want to install." --che
   TRUE "1" "Juju" ""\
   TRUE "2" "LXD" "A hypervisor for LXC, providing fast, secure containers."\
   TRUE "3" "ZFS Utils" "A highly efficient and feature-rich filesystem and logical volume manager."\
-  TRUE "4" "Charm Tools" "The Charm Tools Juju Plugin is a collection of commands enabling users and charm authors to create, search, fetch, update, and manage charms."\
+  FALSE "4" "Charm Tools" "The Charm Tools Juju Plugin is a collection of commands enabling users and charm authors to create, search, fetch, update, and manage charms."\
   --separator=":" --width=750 --height=700)
 
 if [[ $answer =~ "1" ]]; then
